45733f8cfee16ffd0626c4f1f1d38a5b787e8964
Yevgeni Tsodikov <evg.tso@gmail.com>
2021-11-02 16:57:54 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# renovate-common-version
+
+This repository demonstrates how Renovate can create multiple PRs for a single dependency upgrade.
---
+++ src/renovate_common_version/core.clj
@@ -0,0 +1,7 @@
+(ns renovate-common-version.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
